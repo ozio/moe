33f07f7eff3f6d6461c8d3df7626784cda79adc7
oz <i@mr-ozio.ru>
2016-06-29 14:50:08 +0300

    Chai and one more test

--- tests/unit/manager.test.js
+++ tests/unit/manager.test.js
@@ -1,9 +1,17 @@
 import chai from 'chai';
+import should from 'should';
 import { Manager } from '../../src/core/manager';
 
 let assert = chai.assert;
 
-describe('Manager', () => {
+describe('class Manager', () => {
+  it('shouldn\'t be a singleton', () => {
+    let manager1 = new Manager();
+    let manager2 = new Manager();
+
+    assert.equal(manager1 === manager2, false)
+  });
+
   it('should register a value', () => {
     let manager = new Manager();
     let item = { a: 1 };
@@ -28,7 +36,7 @@ describe('Manager', () => {
     assert.equal(manager.exist('Item'), false);
   });
 
-  it('should have right length', () => {
+  it('should have a right length', () => {
     let manager = new Manager();
     let item1 = {};
     let item2 = {};
@@ -36,6 +44,6 @@ describe('Manager', () => {
     manager.register('Item1', item1);
     manager.register('Item2', item2);
 
-    assert.equal(manager.length, 2);
+    manager.should.have.property('length', 2);
   });
 });
