6908be427ab1d19017d3bde27baacce0d4d7c5a8
oz <i@mr-ozio.ru>
2016-07-14 03:57:11 +0300

    Gulp error handling notification fix

--- gulp/tasks/compile.js
+++ gulp/tasks/compile.js
@@ -13,28 +13,39 @@ const exorcist = require('exorcist');
 const headerText = fs.readFileSync(path.join(__dirname, '../header.txt'), 'utf8');
 const licenseText = fs.readFileSync(path.join(__dirname, '../../LICENSE'), 'utf8');
 
+const browserifyConfig = {
+  entries: paths.entry,
+  debug: true,
+  standalone: 'moe',
+};
+
 gulp.task('compile', () => {
-  const stream = browserify({
-    entries: paths.entry,
-    debug: true,
-    standalone: 'moe',
-  })
+  const notifyHandler = notify.onError((error) => {
+    stream.emit('end');
+
+    const rootPath = path.join(__dirname, '../../gamme/').slice(0, -1);
+    const errorString = error.message
+      .split(':')
+      .slice(1)
+      .join(':')
+      .trim()
+      .replace(new RegExp(rootPath, 'g'), '');
+
+    return {
+      title: 'Compile error',
+      message: errorString,
+    };
+  });
+
+  const stream = browserify(browserifyConfig)
     .transform(babelify)
     .bundle()
+    .on('error', notifyHandler)
     .pipe(source('moe.js'))
     .pipe(buffer())
     .pipe(header(headerText, { licenseText, date: new Date().toISOString() }))
     .pipe(transform(() => exorcist('dist/moe.js.map')))
     .pipe(gulp.dest('dist'));
-
-  stream.on('error', notify.onError((error) => {
-    stream.emit('end');
-
-    return {
-      title: '萌え.js — compile error',
-      message: error.message,
-    };
-  }));
 
   return stream;
 });
